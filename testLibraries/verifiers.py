34344367657741c7dbe5e23bf74f66076dfbd3f6
kocipazur <naturat.am@gmail.com>
2016-07-27 22:29:14 +0200

    Added comments

--- testLibraries/verifiers.py
+++ testLibraries/verifiers.py
@@ -1,15 +1,16 @@
 import sys
 
+
 def verify_equals(actual, expected):
-    testFunctionName = sys._getframe().f_code.co_name
-    defaultResult = testFunctionName + " passed."
+    testFunctionName = sys._getframe().f_code.co_name #get current function name
+    defaultResult = testFunctionName + " passed." #set default test result
     result = defaultResult
     try:
-        assert actual == expected
-    except AssertionError:
+        assert actual == expected #actual assertion - check if values are true
+    except AssertionError: #catch Assertion Error if above assert returns false (exception)
         result = testFunctionName + " failed: Expected value: " + str(expected) + " is not equal to actual value: "\
-              + str(actual)
-    except:
+              + str(actual) #assign description of fail
+    except: #catch any other possible error
         result = Exception.message
     print result
 
@@ -38,13 +39,13 @@ def verify_not_equals(actual, expected):
         result = Exception.message
     print result
 
-def verify_returns(testedMethod, methodParameters, expectedType):
+def verify_returns(testedMethod, methodParameters, expectedType): #pass method, args and expected type of result
     testFunctionName = sys._getframe().f_code.co_name
     defaultResult = testFunctionName + " passed."
     result = defaultResult
     try:
-        testedMethodType = type(testedMethod(*methodParameters))
-        assert testedMethodType is expectedType
+        testedMethodType = type(testedMethod(*methodParameters)) #assign method result to variable
+        assert testedMethodType is expectedType #check if assigned variable passes assert
     except AssertionError:
         result = testFunctionName + "failed: Actual type is: " + str(testedMethodType) + " instead of expected: "\
               + str(expectedType)
